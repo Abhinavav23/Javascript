a54159f09f8ed1543771f76fb37ccb05993f550a
Abhinav <abhinav@amits-MacBook-Air.local>
2022-05-29 13:26:22 +0530

    [sesion notes]creating and adding elements in the dom 29th may-2022.

--- dom.js
+++ dom.js
@@ -1,5 +1,5 @@
 /*
-
+use Strict
 console.log(document.URL);
 console.log(document.domain);
 console.log(document.body);
@@ -52,17 +52,171 @@ console.log(cardandtest);
 let input  = document.querySelector('input[type="password"]')
 console.log(input);
 
-*/
+
+
+//sort
+// let arr = [7,2,9,4,8,45,23,11];
+// console.log(arr.sort((x,y)=>y-x));
 
 let selectPara = document.querySelector('p.header-container');
 console.log(selectPara);
-selectPara.innerText = 'this is a changed content'
+console.log(selectPara.innerText);
+
+
+selectPara.innerText = selectPara.innerText +' added Content';
+
+console.log(selectPara.innerText);
 
 let selectedDiv  = document.querySelector('div')
-selectedDiv.innerHTML = '<h3>Abhinav</h3>'
+selectedDiv.innerHTML = '<h2>AV</h2>'
 
 let listItems = document.getElementsByClassName('product')
 
+
+let productList = ['shirt', 'mobile', 'earphone', 'TV'];
+
 for(let i = 0; i<listItems.length;i++){
-    listItems[i]
+    listItems[i].innerText = productList[i] +(i+1);
 }
+
+let div = document.querySelector('div.card.content');
+
+// let footer = document.createTextNode('copyright all right reserved');
+
+// div.appendChild(footer);
+
+let footerEle = document.createElement('p');
+
+footerEle.innerText = 'copyright all right reserved'
+
+footerEle.className = 'myclass';
+footerEle.id = 'myid';
+
+footerEle.style.background = 'red';
+footerEle.style.color = 'red';
+
+div.appendChild(footerEle);
+
+
+
+
+
+let thead  = document.createElement('thead');
+thead.className = 'table-header'
+
+let tbody  = document.createElement('tbody');
+
+console.log(myTable);
+
+myTable.appendChild(thead);
+myTable.appendChild(tbody);
+
+
+console.log(body);
+
+
+
+
+
+
+
+//create table tag
+let myTable = document.createElement('table'); 
+
+console.log();
+
+
+
+
+
+
+
+
+
+//create table header
+let theadhead = document.createElement('thead');
+
+// create table row
+let theadRow= document.createElement('tr');
+
+//function to create th and add data
+function createThandAddData(data){
+    let theadhead = document.createElement('th');
+    theadhead.innerText = data
+    return theadhead
+}
+
+
+
+//function to create td and add data
+function CreateTdAndAddData(data){
+    let td = document.createElement('td');
+    td.innerText = data
+    return td
+}
+
+// crete one tr
+
+// create td with data
+
+// append td with data in tr
+
+
+// create tbody
+
+// append tr in tbody
+
+// append tbody in table
+
+
+
+
+
+theadhead.appendChild(theadRow);
+
+myTable.appendChild(theadhead);
+
+console.log(myTable);
+
+
+let body = document.body
+body.appendChild(myTable)
+
+
+
+
+// css
+// 1. inline style 
+// 2. internal style
+// 3. external style
+
+//create table header
+let table = document.createElement('table');
+
+console.log(table);
+
+let thead = document.createElement('thead')
+
+console.log(thead);
+
+table.appendChild(thead);
+
+let tr = document.createElement('tr');
+
+
+
+thead.appendChild(tr);
+console.log(tr);
+
+let th = document.createElement('th');
+
+tr.appendChild(th);
+
+th.innerText = 'Name'
+
+*/
+
+
+
+
+
