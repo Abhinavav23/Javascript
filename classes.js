97c3d2056823238856af4c36d47c98e16fc3631c
Abhinav <abhinav@amits-MacBook-Air.local>
2022-06-11 12:30:14 +0530

    [sesion notes] Classes, This keyword, getter setter 10th june-2022.

--- classes.js
+++ classes.js
@@ -1,36 +1,174 @@
 
-
+/*
 class Person{
-    static address = 'Earth';
-    houseNo = '42';
-    address = 'Delhi' +houseNo;
+    // static country = 'Earth'
+    country = 'India'
+    static planet = 'Earth'
+   constructor(name, address){
+    this.myName = name,
+    this.myAddress = address
+   }
 
-    constructor(name, birthyear){
-        this.myname = name,
-        this.yob = birthyear
-    }
+   printFullAddress(){
+    console.log(this.myName + ' lives in ' +this.myAddress + this.country);
+   }
 
-    calculateAge = () => {
-        return new Date().getFullYear() - this.yob
-    }
-
-    getAddress(){
-        console.log('Address is Bihar' + address);
-    }
+   static PrintFullDetails(){
+       console.log('This is a person class which allows you to create a person');
+   }
 
 }
 
 
-let abhinav = new Person('Abhinav', 1990);
-console.log(Person.address);
-console.log(abhinav.address);
-console.log(abhinav.calculateAge());
+let av = new Person('Abhinav', 'Delhi');
+// console.log(av);
+// console.log(av.country);
+// av.printFullAddress();
+// av.country = 'USA'
+// av.printFullAddress();
+// console.log(av.planet);
+
+// av.PrintFullDetails();
 
 
-abhinav.address = 'Hyderabad'
-console.log(abhinav.address + abhinav.houseNo);
+// console.log(Person.planet);
+// Person.planet = 'MArs';
+// console.log(Person.planet);
+Person.PrintFullDetails()
 
-console.log(abhinav.getAddress());
+
+// let ram = new Person('Ram', 'Ayodhya');
+// console.log(ram);
+// console.log(ram.country);
+// ram.country = 'USA';
+// console.log(ram.country);
+
+
+
+function CalculateSum(a,b){
+    let first = a
+    let second = b;
+
+}
+
+CalculateSum(2,3);
+
+
+class Player{
+    #speciality = 'Keeper';
+    battingStyle = 'Right handed';
+    constructor(name, game){
+        this.myName = name;
+        this.myGame = game;
+    }
+
+    #PrintSpecilaity(){
+        console.log('speciality is ' +this.#speciality);
+    }
+
+    callPrintSpeciality(val){
+        if(val <10){
+            this.#PrintSpecilaity()
+        }
+    }
+
+    getSpeciality(){
+        this.#PrintSpecilaity()
+        return this.#speciality
+    }
+
+    // method for changing the private property
+    setSpeciality(speciality){
+        if(speciality === 'finisher' | 'captain'){
+            this.#speciality = speciality
+        }
+    }
+}
+
+
+let dhoni = new Player('MS Dhoni', 'cricket');
+console.log(dhoni.battingStyle);
+dhoni.battingStyle = 'left handed';
+console.log(dhoni.speciality);
+console.log(dhoni.getSpeciality());
+dhoni.setSpeciality('finisher');
+console.log(dhoni.getSpeciality());
+dhoni.setSpeciality('bowler');
+console.log(dhoni.getSpeciality());
+
+// this will be undefined hence the result will be 'not a function'
+// dhoni.PrintSpecilaity()
+
+
+
+
+*/
+
+
+
+class ContextClass{
+    value = 'test'
+    shity = ''
+    constructor(name){
+        this.name = name
+    }
+
+    ContextPrint(){
+
+        console.log(this);
+    }
+
+    arrowContextPrint = () => {
+        console.log(this);
+    }
+
+}
+console.log(ContextClass);
+
+
+
+
+
+
+
+
+
+// let context1 = new ContextClass();
+// context1.ContextPrint();
+// context1.arrowContextPrint();
+
+
+
+// let obj = {
+//     test: function(){
+//         console.log(this);
+//         var test1 = () => {
+//             console.log(this);
+//         }
+//         test1()
+//     },
+//     name: 'ram'
+// }
+
+// obj.test()
+
+
+
+function contextCalss(){
+    let a = 10;
+    let b = 10;
+
+    function inner1(){
+        let x = 10;
+        let y =120;
+    }
+}
+
+
+
+
+
+
 
 
 
